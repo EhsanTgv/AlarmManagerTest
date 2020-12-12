d7ae633a4544dc799ce50aba82f9dea96b243ef5
EhsanTgv <EhsanTgv@gmail.com>
2020-12-12 19:11:23 +0330

    Set up BroadcastReceiver

---
+++ app/src/main/java/com/taghavi/alarmmanager/AlarmReceiver.kt
@@ -0,0 +1,16 @@
+package com.taghavi.alarmmanager
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        // Is triggered when alarm goes off, i.e. receiving a system broadcast
+        if (intent.action == "FOO_ACTION") {
+            val fooString = intent.getStringExtra("KEY_FOO_STRING")
+            Toast.makeText(context, fooString, Toast.LENGTH_LONG).show()
+        }
+    }
+}
